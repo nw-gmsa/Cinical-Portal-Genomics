9ad4d71f2917de4ef6e751f62f099ee92ad1d90d
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2026-06-01 09:57:05 +0100

    Initial changes

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,14 +1,10 @@
 const PROXY_CONFIG = {
-  context: [
-    "/healthconnect",
-    "/oauth2",
-    "/login/oauth2",
-    "/exploitation"
-  ],
-  "target": "https://192.168.1.20/",
-  "secure": false,
-  "bypass": function (req, res, proxyOptions) {
-    req.headers["webpass-remote-user"] = "TOTO_USER";
+  "/healthconnect/**" : {
+    "target": "https://192.168.1.20/",
+    "secure": false,
+    "bypass": function (req, res, proxyOptions) {
+      req.headers["webpass-remote-user"] = "TOTO_USER";
+    }
   }
 }
 
